b2cc261e88ced97648cfdc7824eaf2edd79ccc2e
Neil McHenry <neilmchenry@Neils-MacBook-Pro-2.local>
2022-09-10 13:12:57 -0700

    updated database settings

--- gospel_swamp_menu_site/settings.py
+++ gospel_swamp_menu_site/settings.py
@@ -86,17 +86,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'gospel_swamp_menu_site.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -141,5 +130,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Heroku: Update database configuration from $DATABASE_URL.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
+}
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
